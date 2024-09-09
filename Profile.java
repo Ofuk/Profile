b6373b1b58cc01acb7b5d9bea54406edab4cea3c
M. ADLI ULHAQ <162102386+Ofuk@users.noreply.github.com>
2024-09-09 13:37:34 +0700

    Add files via upload

---
+++ Profile.java
@@ -0,0 +1,13 @@
+package MySelf;
+
+public class Profile {
+    public static void main(String[] args) {
+        String Nama = "M. Adli Ulhaq";
+        String Nim = "12350113070";
+        String Hobby = "Playing Video Games, Reading Comics, Singing and Listening Music";
+
+        System.out.println("Nama : " + Nama);
+        System.out.println("NIM : " + Nim);
+        System.out.println("Hobby : " + Hobby);
+    }
+}
