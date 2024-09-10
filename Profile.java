22e7b37b3497d1711e40a1a659c2fbaf27ed8573
M. ADLI ULHAQ <162102386+Ofuk@users.noreply.github.com>
2024-09-10 19:58:34 +0700

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -5,9 +5,12 @@ public class Profile {
         String Nama = "M. Adli Ulhaq";
         String Nim = "12350113070";
         String Hobby = "Playing Video Games, Reading Comics, Singing and Listening Music";
+        String Email = "madliulhaq@gmail.com";
 
         System.out.println("Nama : " + Nama);
         System.out.println("NIM : " + Nim);
         System.out.println("Hobby : " + Hobby);
+        System.out.println("Email : " + Email);
+        
     }
 }
